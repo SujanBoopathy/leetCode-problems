22a32c14a693a77757d49d0bcf2fadac034fb71d
SujanBoopathy <sujansaradha@gmail.com>
2022-04-09 20:51:12 +0530

    leetcode-problem

--- removeDuplicate.java
+++ removeDuplicate.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int k=1;
-        for(int i=1;i<nums.length-1;i++){
+        for(int i=0;i<nums.length-1;i++){
             if(nums[i]!=nums[i+1]){
                 nums[k]=nums[i+1];
                 k++;
