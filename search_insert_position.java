4cd0827150d8282bc80a89a2f73557f0ba51890a
SujanBoopathy <sujansaradha@gmail.com>
2022-04-12 20:02:59 +0530

    problem-solving

--- search_insert_position.java
+++ search_insert_position.java
@@ -1,17 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int data) {
-        int l=0;
+        int mid,l=0;
         int r=nums.length-1;
         
         while(l<r){
-            int mid=(l+r)/2;
+            mid=(l+r)/2;
             if(nums[mid]==data)
                 return mid;
             else if(nums[mid]<data){
-                r=mid-1;
+                l=mid+1;
             }
             else{
-                l=mid+1;
+                r=mid-1;
             }
         }
         return -1;
