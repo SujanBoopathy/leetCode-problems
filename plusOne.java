482e2b5d796493c18e0d8e97349f3126047f7502
SujanBoopathy <sujansaradha@gmail.com>
2022-04-15 16:52:59 +0530

    leetcode-problem

--- plusOne.java
+++ plusOne.java
@@ -1,7 +1,5 @@
-import java.util.*;
-public class plusOne {
-
-    public static int[] plusOnew(int[] digits) {
+class Solution {
+    public int[] plusOne(int[] digits) {
         int len=digits.length;
         for(int i=len-1;i>=0;i--){
             if(digits[i]<9){
@@ -15,19 +13,5 @@ public class plusOne {
         int na[]=new int[len+1];
         na[0]=1;
         return na;
-    }
-    
-
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a[]=new int[3];
-        for(int i=0;i<3;i++){
-            a[i]=sc.nextInt();
-        }
-        int b[]=new int[4];
-        b=plusOnew(a);
-        for(int i=0;i<4;i++){
-            System.out.println(b[i]);
-        }
     }
 }
