1dc66146061896cd93544705aa3522279e8beecd
SujanBoopathy <sujansaradha@gmail.com>
2022-04-23 20:11:38 +0530

    leetcode-problem

--- valid_palindrome.cpp
+++ valid_palindrome.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    bool checkPalindrome(string s){
-        int len=s.length();
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=s[len-i-1])
-                return false;
-        }
-        return true;
-    }
     bool isPalindrome(string s) {
-        if(s==" ")
-            return true;
         string str="";
         for(int i=0;i<s.length();i++){
-            s[i]=tolower(s[i]);
-            if(s[i]>='a' && s[i]<='z'){
-                str+=s[i];
+            if(isalnum(s[i])){
+                str+=tolower(s[i]);
             }
         }
-        return checkPalindrome(str);
+        string rev=str;
+        reverse(rev.begin(),rev.end());
+        if(str==rev)
+            return true;
+        else 
+            return false;
     }
 };
