fb823bec99cbd8d88b608d8ea014b0c9dccbfdbe
SujanBoopathy <sujansaradha@gmail.com>
2022-04-25 21:03:46 +0530

    problem_solving

---
+++ day_of_the_year.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public int dayOfYear(String date) {
+        String s[]=date.split("-");
+        int year=Integer.parseInt(s[0]);
+        int noOfDays=Integer.parseInt(s[2]);
+        int month=Integer.parseInt(s[1]);
+        int sum=0;
+        int m[]={31,28,31,30,31,30,31,31,30,31,30,31};
+        for(int i=0;i<month-1;i++){
+            sum+=m[i];
+        }
+        noOfDays+=sum;
+        if(month>2 && year%4==0 )){
+            return noOfDays+1;
+        }
+        else{
+            return noOfDays;
+        }
+    }
+}
