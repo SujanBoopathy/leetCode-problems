e4959d8e1810826ddb534b19928f40e9bf15cda7
SujanBoopathy <sujansaradha@gmail.com>
2022-04-28 20:37:42 +0530

    problem-solving

---
+++ unique_number_of_occurence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        vector<int>v;
+        for(auto m: mp){
+            v.push_back(m.second);
+        }
+        sort(v.begin(),v.end());
+        for(int i=1;i<v.size();i++){
+            if(v[i]==v[i-1])
+                return false;
+        }
+        return true;
+    }
+};
