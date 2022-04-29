025fdd657dc9ba837b68456e067d43eb80406a07
SujanBoopathy <sujansaradha@gmail.com>
2022-04-29 22:51:06 +0530

    problem_solving

--- intersection_of_two_array.cpp
+++ intersection_of_two_array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        /*
         set<int> s1(nums1.begin(),nums1.end());
         set<int> s2(nums2.begin(),nums2.end());
         vector<int> v;
@@ -11,5 +12,22 @@ public:
             }
         }
         return v;
+        */
+        if(nums1.size()>nums2.size()){
+            intersection(nums2,nums1);
+        }
+        unordered_map<int,int> mp;
+        for(auto i : nums1){
+            mp[i]=1;
+        }
+        vector<int> v;
+        for(auto i:nums2){
+            if(mp[i]==1){
+                v.push_back(i);
+                mp[i]--;
+            }
+        }
+        return v;
+        
     }
 };
