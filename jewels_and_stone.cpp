a5d6dfe14bdb014618e7467bd778f455431efcbf
SujanBoopathy <sujansaradha@gmail.com>
2022-04-30 21:58:24 +0530

    problem-solving

---
+++ jewels_and_stone.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(char c :jewels){
+            for(char s:stones){
+                if(c==s)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
