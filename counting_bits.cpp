a29236996dac293e5003806fc9342b759e58c379
SujanBoopathy <sujansaradha@gmail.com>
2022-04-30 22:27:37 +0530

    problem-solving

---
+++ counting_bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] countBits(int n) {
+        String s[]=new String[n];
+        for(int i=0;i<=n;i++){
+            s[i]=Integer.toBinaryString(i);
+        }
+        List<Integer> lst=new ArrayList<Integer>();
+        for(int i=0;i<=n;i++){
+            String temp=s[i];
+            int c=0;
+            for(int j=0;j<temp.length();j++){
+                if(temp.charAt(j)=='1')
+                    c++;
+            }
+            lst.add(c);
+        }
+        int[] r=lst.toArray(new int[lst.size()]);
+        return r;
+        
+    }
+}
