4428926964e9facc5ebfa5faa0f1a71596929bc1
SujanBoopathy <sujansaradha@gmail.com>
2022-05-01 22:41:15 +0530

    problem-solving

--- find_all_disappered_element.cpp
+++ find_all_disappered_element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        set<int> s(nums.begin(),nums.end());
+        unordered_set<int> s(nums.begin(),nums.end());
         int n=nums.size();
         vector<int> v;
         for(int i=1;i<=n;i++){
