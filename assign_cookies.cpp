cfd6af6ded7f4d7a2e88f71715182cfa9ddab42a
SujanBoopathy <sujansaradha@gmail.com>
2022-05-02 18:13:28 +0530

    problem-solving

--- assign_cookies.cpp
+++ assign_cookies.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        unordered_map<int,int> mp;
-        int count=0;
-        for(auto a :g){
-            mp[a]++;
-        }
-        for(auto a:s){
-            if(mp[a]!=0){
-                mp[a]--;
-                count++;
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int noOfCookies=s.size(),noOfKids=g.size(),ans=0,cookies=0;
+        for(int i=0;i<noOfKids && cookies<noOfCookies;){
+            if(s[cookies]>=g[i]){
+                i++;
+                ans++;
             }
+            cookies++;
         }
-        return count;
+        return ans;
     }
 };
