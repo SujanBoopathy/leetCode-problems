cc51035c8ba93aefdbbc7804279aea77c3866f59
SujanBoopathy <sujansaradha@gmail.com>
2022-05-02 18:20:53 +0530

    problem-solving

--- number_of_segment_in_a_string.cpp
+++ number_of_segment_in_a_string.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    bool checkStr(string t){
+        for(int i=0;i<t.length();i++){
+            if(t[i]!=32)
+                return true;
+        }
+        return false;
+    }
     int countSegments(string s) {
-        if(s=="" || s==" ") return 0;
         string temp="";
         vector<string> v;
         for(int i=0;i<s.length();i++){
-            if(s[i]==32){
+            if(s[i]==32 && checkStr(temp)){
                 v.push_back(temp);
             }
             else{
                 temp+=s[i];
             }
         }
-        v.push_back(temp);
+        if(checkStr(temp))
+            v.push_back(temp);
         return v.size();
     }
 };
