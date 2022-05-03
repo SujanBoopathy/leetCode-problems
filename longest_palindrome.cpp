22b7beed282b50d3da94ac0714f56a87f7f3a230
SujanBoopathy <sujansaradha@gmail.com>
2022-05-03 12:02:05 +0530

    problem_solving

--- longest_palindrome.cpp
+++ longest_palindrome.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int longestPalindrome(string s){
-        
+        int a[52]={0};
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='a' && s[i]<='z'){
+                a[s[i]-'a']++;
+            }
+            else{
+                a[s[i]-'a'+26]++;
+            }
+        }
+        int count=0;
+        for(int i=0;i<52;i++){
+            count+=a[i]/2;
+        }
+        count*=2;
+        return count+1;
     }
 };
