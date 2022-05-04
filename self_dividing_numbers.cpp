c71afb929bc5645c68c219044892e0f033361abf
SujanBoopathy <sujansaradha@gmail.com>
2022-05-04 19:02:58 +0530

    problem-solving

--- self_dividing_numbers.cpp
+++ self_dividing_numbers.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
-    
+    bool check(int x){
+        int temp,r=x;
+        while(x>0){
+            temp=x%10;
+            if(temp==0) return false;
+            if(r%temp!=0) return false;
+            x/=10;
+        }
+        return true;
+    }
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int> v;
         for(int i=left;i<=right;i++){
