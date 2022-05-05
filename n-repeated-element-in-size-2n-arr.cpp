33321b384867716a6c68855b91b095d6c471887c
SujanBoopathy <sujansaradha@gmail.com>
2022-05-05 20:07:49 +0530

    problem_solving

--- n-repeated-element-in-size-2n-arr.cpp
+++ n-repeated-element-in-size-2n-arr.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+         unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
         }
+        for(auto& pair:mp){
+            if(pair.second>1)
+                return pair.first;
+        }
+        return 0;
         return 0;
     }
 };
