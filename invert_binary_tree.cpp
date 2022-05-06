0fd34d04da68b4d894f91bbf786e980bde0a0b08
SujanBoopathy <sujansaradha@gmail.com>
2022-05-06 19:12:32 +0530

    problem_solving

--- invert_binary_tree.cpp
+++ invert_binary_tree.cpp
@@ -11,7 +11,13 @@
  */
 class Solution {
 public:
-    
+    void invert(TreeNode* root){
+        if(root==NULL)
+            return ;
+        swap(root->left,root->right);
+        invert(root->left);
+        invert(root->right);
+    }
     TreeNode* invertTree(TreeNode* root) {
         invert(root);
         return root;
