cfb3ebea1e98939e77a68cc7980645a5c0a27dac
SujanBoopathy <sujansaradha@gmail.com>
2022-05-12 20:26:50 +0530

    problem_solving

---
+++ maximum_product_of_three_numbers.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        return nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3];
+    }
+};
