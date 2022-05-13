e4c0c3133064a6deddb53073de2b1b0d537589c0
SujanBoopathy <sujansaradha@gmail.com>
2022-05-13 23:33:58 +0530

    problem-solving

--- sort_array_by_parity.cpp
+++ sort_array_by_parity.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int start=0,end=nums.size()-1;
-        while(start<end){
-            while(nums[start]%2==0 && start<nums.size())
-                start++;
-            while(nums[end]%2==1 && end>=0)
-                end--;
-            if(nums[start]%2==1 && nums[end]%2==0 && start<nums.size() && end>=0)
-                swap(nums[start++],nums[end--]);
+        vector<int> v;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0)
+                v.push_back(nums[i]);
         }
-        return nums;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==1)
+                v.push_back(nums[i]);
+        }
+        return v;
+        
     }
 };
