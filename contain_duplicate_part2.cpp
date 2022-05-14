9f3348c091e2ab4fc81635f270334a149e319919
SujanBoopathy <sujansaradha@gmail.com>
2022-05-14 21:36:39 +0530

    problem_solving

--- contain_duplicate_part2.cpp
+++ contain_duplicate_part2.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j] && abs(i-j)<=k)
-                    return true;
+            if(mp.find(nums[i])==mp.end()){
+                mp[nums[i]]=i;
+            }
+            else{
+               if(abs(mp[nums[i]]-i)<=k)
+                   return true;
+                else{
+                    mp[nums[i]]=i;
+                }
             }
         }
         return false;
