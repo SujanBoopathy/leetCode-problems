9dc90a0ee06be54b74329e697a63fe2051faa3fe
SujanBoopathy <sujansaradha@gmail.com>
2022-05-14 20:35:39 +0530

    problem-solving

--- reverse_bits.cpp
+++ reverse_bits.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         string s="";
-        for(int i=0;i<32;i++){
-            int temp=n%2;
+        while(n!=0){
+            s+=to_string(n%2);
             n/=2;
-            s+=(char)temp;
         }
+        string t(32-s.size(),0);
+        s+=t;
         uint32_t res;
-        for(int i=0;i<32;i++){
-            res=res*10+int(s[i]);
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='1')
+                res+=pow(2,s.size()-i-1);
         }
         return res;
     }
