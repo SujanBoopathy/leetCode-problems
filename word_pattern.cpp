e3af2f551be8167f5ab62b387fd8c28395ee4797
SujanBoopathy <sujansaradha@gmail.com>
2022-05-14 21:59:17 +0530

    problem_solving

--- word_pattern.cpp
+++ word_pattern.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        string temp="",res="";
-        vector<string> v;
-        set<char> st;
-        for(int i=0;i<pattern.length();i++){
-            st.insert(s[i]);
-        }
-        for(int i=0;i<s.length();i++){
-            temp+=s[i];
-            if(s[i]==32){
-                v.push_back(temp);
-                temp=" ";
-            } 
-        }
-        set<string> st2(v.begin(),v.end());
+        string res="";
+        set<string> v;
         unordered_map<char,string> mp;
-        if(st.size()!=st2.size)
-            return false;
-        else{
-            set<int>::iterator itr1,itr2;
-            for(itr1=st.begin(),itr2=st2.begin();itr1!=st.end() && itr2!=st2.end();itr1++,itr2++){
-                mp[itr1]=itr2;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '){
+                v.insert(res);
+                res="";
+            }
+            else 
+                 res+=s[i];
+        }
+        v.insert(res);
+        for(int i=0;i<pattern.length();i++){
+            if(mp.find(pattern[i])!=mp.end()){
+                if(mp[pattern[i]]!=v[i])
+                    return false;
+            }
+            else{
+                mp[pattern[i]]=v[i];
             }
         }
+        res="";
         for(int i=0;i<pattern.length();i++){
-            
+            res+=mp[pattern[i]];
+            if(i<pattern.length()-1)
+                res+=' ';
         }
-        
+        if(res==s)
+            return true;
+        else
+            return false;
+
     }
 };
