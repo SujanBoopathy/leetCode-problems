3a1b6b1b0200e4d00e497d4fddecfb88bf3d4a8b
SujanBoopathy <sujansaradha@gmail.com>
2022-05-15 13:42:35 +0530

    problem-solving

--- guess_num_higher_or_lower.cpp
+++ guess_num_higher_or_lower.cpp
@@ -10,12 +10,17 @@
 class Solution {
 public:
     int guessNumber(int n) {
+        //binary search method
+        // guess function returns
+        // 1 if n is less than pick
+        // -1 if n is greater than pick
+        // 0 if n is equal to pick
         int left=1,right=n;
         while(left<=n){
             int mid=left+(right-left)/2;
             if(guess(mid)==0)
                 return mid;
-            else if(guess(mid)==-1)
+            else if(guess(mid)==1)
                 left=mid+1;
             else
                 right=mid-1;
