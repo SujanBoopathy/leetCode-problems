6310e2922a60374e1d15bd4b3ea63da926833460
SujanBoopathy <sujansaradha@gmail.com>
2022-05-15 13:00:47 +0530

    problem_solving

---
+++ ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[256]={0};
+        for(int i=0;i<ransomNote.length();i++){
+            count[ransomNote[i]]++;
+        }
+        for(int i=0;i<magazine.length();i++){
+            if(count[magazine[i]]>0){
+                count[magazine[i]]--;
+            }
+        }
+        for(int i=0;i<256;i++){
+            if(count[i]!=0)
+                return false;
+        }
+        return true;
+        
+    }
+};
