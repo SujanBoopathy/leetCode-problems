9d0ab6ed06b9f941fe5e9bcdaa3ffe1a596b6674
SujanBoopathy <sujansaradha@gmail.com>
2022-05-15 13:41:26 +0530

    problem-solving

--- guess_num_higher_or_lower.cpp
+++ guess_num_higher_or_lower.cpp
@@ -15,7 +15,7 @@ public:
             int mid=left+(right-left)/2;
             if(guess(mid)==0)
                 return mid;
-            else if(guess(mid)==1)
+            else if(guess(mid)==-1)
                 left=mid+1;
             else
                 right=mid-1;
