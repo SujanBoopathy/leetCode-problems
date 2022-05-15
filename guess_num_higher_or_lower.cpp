c12ca55b83573245af96b830fcf34024b793dd73
SujanBoopathy <sujansaradha@gmail.com>
2022-05-15 13:06:57 +0530

    problem_solving

---
+++ guess_num_higher_or_lower.cpp
@@ -0,0 +1,26 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int i=n;
+        while(guess(n)==0){
+            if(guess(n)==1){
+                n++;
+                i=n;
+            }
+            else{
+                n--;
+                i=n;
+            }
+        }
+        return i;
+    }
+};
