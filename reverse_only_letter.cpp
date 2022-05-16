a8205e3c4a46341aa5517a39a34a5e4786af2558
SujanBoopathy <sujansaradha@gmail.com>
2022-05-16 21:52:25 +0530

    problem_solving

--- reverse_only_letter.cpp
+++ reverse_only_letter.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    bool check(char c){
-        if(c>='a' && c<='z')
-            return true;
-        if(c>='A' && c<='Z')
-            return true;
-        return false;
-    }
+   
     string reverseOnlyLetters(string s) {
         int start=0,end=s.length()-1;
         while(start<end){
