a4450a4323faeef8fda7491d2e3488bf3c94b6fb
SujanBoopathy <sujansaradha@gmail.com>
2022-05-18 21:14:14 +0530

    problem_solving

--- set_mismatch.cpp
+++ set_mismatch.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<int> v;
-        for(int i=1;i<nums.size();i++){
+        int n=nums.size();
+        int sum=(n*(n+1))/2;
+        int chk=0;
+        for(int i=1;i<n;i++){
             if(nums[i]==nums[i-1]){
                 v.push_back(nums[i]);
-                v.push_back(nums[i]+1);
-                break;
+            }
+            else{
+                chk+=nums[i-1];
             }
         }
+        chk+=nums[n-1];
+        v.push_back(sum-chk);
         return v;
     }
 };
