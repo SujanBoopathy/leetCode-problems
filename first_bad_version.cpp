9634b8d36c58e76641332b9eb10197b255ac9086
SujanBoopathy <sujansaradha@gmail.com>
2022-05-20 21:26:09 +0530

    problem_solving

--- first_bad_version.cpp
+++ first_bad_version.cpp
@@ -7,20 +7,20 @@ public:
         if(n==1) return 1;
         int mid=n/2;
         if(!isBadVersion(mid)){
+            if(!isBadVersion(mid-1) && mid-1>0){
+                if(isBadVersion(mid+1) && mid+1<=n){
+                    return mid;
+                }
+            }
+            
             for(int i=mid+1;i<=n;i++){
                 if(isBadVersion(i)){
                     return i;
                 }
             }
         }
-        else if(isBadVersion(mid)){
-            if(!isBadVersion(mid-1) && mid-1>0){
-                if(isBadVersion(mid+1) && mid+1<=n){
-                    return mid;
-                }
-            }
-        }
         else{
+            
             for(int i=mid-1;i>0;i--){
                 if(!isBadVersion(i)){
                     return i+1;
