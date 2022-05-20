d812eb806d66f8b2c84e230f548fb3fe53253371
SujanBoopathy <sujansaradha@gmail.com>
2022-05-20 21:33:17 +0530

    problem_solving

--- first_bad_version.cpp
+++ first_bad_version.cpp
@@ -4,26 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        if(n==1) return 1;
-        if(isBadVersion(1)) return 1;
-        int mid=n/2;
-        if(!isBadVersion(mid)){           
-            
-            for(int i=mid+1;i<=n;i++){
-                if(isBadVersion(i)){
-                    return i;
-                }
+        int left=1,right=n;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(isBadVersion(mid)){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
             }
         }
-        else{
-            
-            for(int i=mid-1;i>0;i--){
-                if(!isBadVersion(i)){
-                    return i+1;
-                }
-            }
-        }
-        return -1;
-        
+        return left;
     }
 };
