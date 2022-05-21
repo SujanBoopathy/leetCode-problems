1482c2318c87240f0bea3001bd6eddc3b3ee2dbe
SujanBoopathy <sujansaradha@gmail.com>
2022-05-21 21:26:33 +0530

    problem_solving

--- island_perimeter.cpp
+++ island_perimeter.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        
+        int perimeter=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();i++){
+                if(grid[i][j]==1){
+                    if(i-1>=0 && i<=grid.size()){
+                        if(grid[i-1][j]==0){
+                            perimeter++;
+                        }
+                        if(grid[i+1][j]==0)
+                            perimeter++;
+                    }
+                }
+            }
+        }
     }
 };
