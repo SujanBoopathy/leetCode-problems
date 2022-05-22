a47b86e3886f1ef4e9b388ba404d98f6532ed12a
SujanBoopathy <sujansaradha@gmail.com>
2022-05-22 23:44:40 +0530

    problem_solving

--- maximum_average_subarray.cpp
+++ maximum_average_subarray.cpp
@@ -10,8 +10,11 @@ public:
         return res/k;
     }
     double findMaxAverage(vector<int>& nums, int k) {
+        if(nums.size()==1){
+            return nums[0];
+        }
         double res=0.0;
-        for(int i=0;i<nums.size()-k;i++){
+        for(int i=0;i<=nums.size()-k;i++){
             double temp=calculate(nums,k,i);
             if(temp>res){
                 res=temp;
