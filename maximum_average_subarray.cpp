c373e41d9346d0c33951d40f6bbf5b85559cc6db
SujanBoopathy <sujansaradha@gmail.com>
2022-05-22 23:54:28 +0530

    problem_solving

--- maximum_average_subarray.cpp
+++ maximum_average_subarray.cpp
@@ -1,41 +1,13 @@
 class Solution {
 public:
-    bool ok(double x, vector <int>& nums, int k){
-      int n = nums.size();
-      double arr[n];
-      for (int i = 0; i < n; i++) {
-         arr[i] = nums[i] - x;
-      }
-      double sum = 0;
-      double last = 0;
-      for (int i = 0; i < k; i++) {
-         sum += arr[i];
-      }
-      if (sum >= 0)
-      return true;
-      for (int i = 0, j = k; j < n; i++, j++) {
-         last += arr[i];
-         sum += arr[j];
-         if (last < 0) {
-            sum -= last;
-            last = 0;
-         }
-         if (sum >= 0)
-         return true;
-      }
-      return false;
-   }
     double findMaxAverage(vector<int>& nums, int k) {
-        if(nums.size()==1){
-            return nums[0];
+        int sum=accumulate(nums.begin(),nums.begin()+k,0);
+        int max_sum=sum;
+        for(int i=k;i<nums.size();i++){
+            sum=sum+nums[i]-nums[i-k];
+            if(max_sum<sum)
+                max_sum=sum;
         }
-        double res=0.0;
-        for(int i=0;i<=nums.size()-k;i++){
-            double temp=calculate(nums,k,i);
-            if(temp>res){
-                res=temp;
-            }
-        }
-        return res;
+        return static_cast<double>(max_sum)/k;
     }
 };
