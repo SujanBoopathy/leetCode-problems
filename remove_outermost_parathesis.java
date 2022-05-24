e18e41b89416e0329a307738c0c1b21288d18c26
SujanBoopathy <sujansaradha@gmail.com>
2022-05-24 21:45:37 +0530

    problem_solving

--- remove_outermost_parathesis.java
+++ remove_outermost_parathesis.java
@@ -1,5 +1,23 @@
 class Solution {
-    public String removeOuterParentheses(String s){
+    public String removeOuterParentheses(String s) {
+        String res="";
+        int check=0;
+        List<String> arr=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                check++;
+            }
+            else if(s.charAt(i)==')'){
+                check--;
+            } 
+            
+                       
+        }
+        String result="";
+        for(String aa:arr){
+            result+=aa;
+        }
         
+        return result;
     }
 }
