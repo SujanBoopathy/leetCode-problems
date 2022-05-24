3b8f7069539ed257b2001acad38e706aa91e0aa3
SujanBoopathy <sujansaradha@gmail.com>
2022-05-24 21:49:31 +0530

    problem_solving

--- square_of_sorted_array.java
+++ square_of_sorted_array.java
@@ -1,3 +1,9 @@
-public class square_of_sorted_array {
-    
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
 }
