93cae65abf20e5a7f35c153c36efb19282b7f314
SujanBoopathy <sujansaradha@gmail.com>
2022-05-26 22:49:42 +0530

    problem-solving

--- reshape_the_matrix.java
+++ reshape_the_matrix.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int totalElements = mat.length * mat[0].length;
+        if (totalElements != r * c || totalElements % r != 0) {
+            return mat;
+        }
         List<Integer> list=new ArrayList<>();
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[i].length;j++){
@@ -7,10 +11,11 @@ class Solution {
             }
         }
         int res[][]=new int[r][c];
-        ListIterator<Integer> itr=list.listIterator<>();
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                res[i][j]=itr.next();
+        
+        int temp=0;
+        for(int i=0;i<r && list.size()>0;i++){
+            for(int j=0;j<c && list.size()>0;j++){
+                res[i][j]=list.get(temp++);
             }
         }
         return res;
