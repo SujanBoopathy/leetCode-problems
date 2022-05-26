59dd6e0367509bd9830f36081a669ecdc6f22b6b
SujanBoopathy <sujansaradha@gmail.com>
2022-05-26 22:26:40 +0530

    problem_solving

--- longest_common_subsequence.java
+++ longest_common_subsequence.java
@@ -9,7 +9,13 @@ class Solution {
                 map.put(n,map.get(n)+1);
         }
         int maxValue=0;
-         
+        for(Map.Entry<Integer,Integer> mp:map.entrySet()){
+            if(map.containsKey(mp.getKey()+1)){
+                int maxTemp=mp.getValue()+map.get(mp.getKey()+1);
+                if(maxTemp>maxValue)
+                    maxValue=maxTemp;
+            }
+        }  
         return maxValue;
     }
 }
