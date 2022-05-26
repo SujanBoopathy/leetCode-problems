14ecfe351e7e653aafd14a66b7ac9690d4db2fdb
SujanBoopathy <sujansaradha@gmail.com>
2022-05-26 22:15:51 +0530

    problem_solving

--- longest_common_subsequence.java
+++ longest_common_subsequence.java
@@ -1,16 +1,5 @@
 class Solution {
     public int findLHS(int[] nums) {
-        Arrays.sort(nums);
-        int start=0,end+0;
-        int count=1,maxCount=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                count++;
-            }
-            if(count>maxCount){
-                maxCount=count;
-                
-            }
-        }
+        
     }
 }
