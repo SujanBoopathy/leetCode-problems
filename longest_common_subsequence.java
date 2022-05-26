deb7053d0cdbea6f2ebfdecf710e68122517b3f1
SujanBoopathy <sujansaradha@gmail.com>
2022-05-26 22:26:17 +0530

    problem_solving

--- longest_common_subsequence.java
+++ longest_common_subsequence.java
@@ -1,5 +1,15 @@
 class Solution {
     public int findLHS(int[] nums) {
-        
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int n:nums){
+            if(!map.containsKey(n)){
+                map.put(n,1);
+            }
+            else{
+                map.put(n,map.get(n)+1);
+        }
+        int maxValue=0;
+         
+        return maxValue;
     }
 }
