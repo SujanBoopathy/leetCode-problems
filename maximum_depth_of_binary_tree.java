385c7563c633db614b26fc9757ba75cbe194d592
SujanBoopathy <sujansaradha@gmail.com>
2022-05-27 22:50:17 +0530

    problem-solving

--- maximum_depth_of_binary_tree.java
+++ maximum_depth_of_binary_tree.java
@@ -14,3 +14,20 @@
  *     }
  * }
  */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        else{
+        int ldepth=maxDepth(root.left);
+        int rdepth=maxDepth(root.right);
+        
+        if(ldepth<rdepth){
+            return rdepth+1;
+        }
+        else{
+            return ldepth+1;
+        }
+        }
+    }
+}
