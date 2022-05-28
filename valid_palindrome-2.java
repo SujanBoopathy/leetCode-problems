8b1c9ac36190f5a54c4d6dbfa945601e0cf4472c
SujanBoopathy <sujansaradha@gmail.com>
2022-05-28 23:26:14 +0530

    problem_solving

--- valid_palindrome-2.java
+++ valid_palindrome-2.java
@@ -1,24 +1,25 @@
 class Solution {
-    public boolean check(String str){
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
-                return false;
-            }
-        }
-        return true;
-    }
     public boolean validPalindrome(String s) {
-        if(check(s) || s.length()<=2)
-            return true;
+        
         for(int i=0;i<s.length();i++){
-            String temp=s.substring(0,i)+s.substring(i+1,s.length());
-            
-            if(temp.length()<=2)
-                return false;
-            if(check(temp)){
+            int start=0;
+            int end=s.length()-1;
+            while(start<end){
+                if(start==i){
+                     start++; break;
+                }
+                if(end==i){
+                    end--; break;
+                }
+                if(s.charAt(start)!=s.charAt(end)){
+                    break;
+                }
+                start++;
+                end--;
+            }
+            if(start>=end){
                 return true;
             }
-            
         }
         return false;
     }
