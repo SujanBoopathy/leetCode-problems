99d6dc146035c906fd31f459f2ddeb4770f3597e
SujanBoopathy <sujansaradha@gmail.com>
2022-05-28 22:55:57 +0530

    problem_solving

--- detect_capital.java
+++ detect_capital.java
@@ -14,6 +14,12 @@ class Solution {
             if(word.length()>1){
                 temp=word.charAt(1);
             }
+            if(temp>='A' && temp<='Z'){
+                for(int i=0;i<word.length();i++){
+                    if(word.charAt(i)<='z' && word.charAt(i)>='a')
+                        return false;
+                }
+            }
             
         }
         return true;
