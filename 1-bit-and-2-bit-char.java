ed670892afd4183b9151133caba4ea3dcd4e8a18
SujanBoopathy <sujansaradha@gmail.com>
2022-05-29 20:57:15 +0530

    problem_solving

--- 1-bit-and-2-bit-char.java
+++ 1-bit-and-2-bit-char.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-        
+        int flag=0;
+        for(int i=0;i<bits.length;i++){
+            if(bits[i]==0){
+                flag=1;
+            }
+            else{
+                flag=0;
+                i++;
+            }
+        }
+        if(flag==1) return true;
+        else return false;
     }
 }
