136fb2ce234486bcba069feda4445fd22eb5417f
SujanBoopathy <sujansaradha@gmail.com>
2022-05-29 21:26:35 +0530

    problem_solving

---
+++ toeplitz_matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        for(int index=0;index<n;index++){
+            int i=0;
+            int j=index;
+            int value=matrix[i][j];
+            while(i<m && j<n){
+                if(matrix[i][j]!=value)
+                    return false;
+                i++;
+                j++;
+            }
+        }
+        
+        return true;
+    }
+}
