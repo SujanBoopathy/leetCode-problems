24b472086444590a5b6f694d2e584863f39511de
SujanBoopathy <sujansaradha@gmail.com>
2022-05-31 23:04:04 +0530

    problem_solving

--- keyboard-row.java
+++ keyboard-row.java
@@ -3,34 +3,34 @@ class Solution {
         String firstRow="qwertyuiopQWERTYUIOP";
         String secondRow="asdfghkjlASDFGHJKL";
         String thirdRow="zxcvbnmZXCVBNM";
-
-
+        
         Map<Character,Integer> rowId=new HashMap<>();
         List<String> list=new ArrayList<>();
-
+        
         for(int i=0;i<firstRow.length();i++)
             rowId.put(firstRow.charAt(i),1);
         for(int i=0;i<secondRow.length();i++)
             rowId.put(secondRow.charAt(i),2);
         for(int i=0;i<thirdRow.length();i++)
             rowId.put(thirdRow.charAt(i),3);
-
-            for(int ind=0;ind<words.length;ind++){
-                String temp=words[ind];
-                boolean flag=true;
-                for(int i=0;i<temp.length();i++){
-                    if(rowId.get(temp.charAt(i)).equals(rowId.get(temp.charAt(0)))){
-                        continue;
-                    }
-                    else{
-                        flag=false;
-                        break;
-                    }
+        
+        for(int ind=0;ind<words.length;ind++){
+            String temp=words[ind];
+            boolean flag=true;
+            for(int i=0;i<temp.length();i++){
+                if(rowId.get(temp.charAt(i)).equals(rowId.get(temp.charAt(0)))){
+                    continue;
                 }
-                if(flag){
-                    list.add(temp);
+                else{
+                    flag=false;
+                    break;
                 }
             }
-}
-
+            if(flag){
+                list.add(temp);
+            }
+        }
+    
+        return list.toArray();
+    }
 }
