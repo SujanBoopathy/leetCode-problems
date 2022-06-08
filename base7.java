4b22363311aa61a62398eba5d94468ff17fe023d
SujanBoopathy <sujansaradha@gmail.com>
2022-06-08 21:16:26 +0530

    problem-solving

--- base7.java
+++ base7.java
@@ -1,7 +1,9 @@
 class Solution {
     public String convertToBase7(int num) {
-         if(num<0) return "-"+convertToBase7(-num);
-        else if(num<7) return Integer.toString(num);
+         if(num<0)
+             return "-"+convertToBase7(-num);
+        else if(num<7) 
+            return Integer.toString(num);
         else
            return  convertToBase7(num/7) + convertToBase7(num%7);
     }
