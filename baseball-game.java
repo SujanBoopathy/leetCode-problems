e71e8e818080be21002181fab8e002388a5707ba
SujanBoopathy <sujansaradha@gmail.com>
2022-06-09 21:40:32 +0530

    problem-solving

--- baseball-game.java
+++ baseball-game.java
@@ -1,3 +1,18 @@
-public class baseball-game {
-    
+class Solution {
+    public int calPoints(String[] ops) {
+        Stack<Integer> stk=new Stack<>();
+        int totalScore=0;
+        for(int i=0;i<ops.length;i++){            
+            if(ops[i].equals("+")){
+                int scoreTwo=stk.pop();
+                int newScore=scoreTwo+stk.peek();
+                stk.push(scoreTwo);
+                stk.push(newScore);
+                totalScore+=newScore;
+            }
+           
+        }
+        
+        return totalScore;
+    }
 }
