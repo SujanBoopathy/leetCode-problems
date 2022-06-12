5f5bc15732f79a5e7267affc85a0dec8d0f10108
SujanBoopathy <sujansaradha@gmail.com>
2022-06-12 18:32:10 +0530

    problem-solving

--- pmain.py
+++ pmain.py
@@ -1,11 +1,18 @@
 string1=input("Enter string:")
 low=""
 high=""
+digi=""
+specialchar=""
 for i in range(len(string1)):
     temp=str(string1[i])
     if(temp.islower()):
         low+=str(temp)
-    else:
+    elif(temp.isupper()):
         high+=str(temp)
+    elif(temp.isdigit()):
+        digi+=str(temp)
+    else:
+        specialchar+=str(temp)
 
-print(low+high)
+
+print(low+high+digi+specialchar)
