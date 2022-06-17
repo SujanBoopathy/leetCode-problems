d2f51e9e546e549371d4bc545ed18ff552e478b7
SujanBoopathy <sujansaradha@gmail.com>
2022-06-17 21:01:16 +0530

    problem-solving

--- remove-adj-duplicate-in-str.cpp
+++ remove-adj-duplicate-in-str.cpp
@@ -4,9 +4,10 @@ public:
         int start=0,end=s.length();
         while(start<=end){
             if(s[start]==s[start+1]){
-                s=s.substr(0,start+1)+s.substr(start+2,end+1);
+                s=s.substr(0,start)+s.substr(start+2,end-1);
                 end--;
                 start=0;
+                continue;
             }
             start++;
         }
