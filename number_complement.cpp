162d14176739cb0ddeccc77f27b9031ac133dfb9
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-07-22 23:32:53 +0530

    Update number_complement.cpp

--- number_complement.cpp
+++ number_complement.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int findComplement(int num) {
-        string s="";
+        string str="";
         while(num>=2){
             if(num%2==1)
-                s+="0";
+                str+="0";
             else
-                s+="1";
+                str+="1";
             num/=2;
         }
-        s+=num;
+        str+=num;
         int sum=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='1'){
+        for(int i=0;i<str.length();i++){
+            if(str[i]=='1'){
                 sum+=pow(2,i);
             }                
         }
