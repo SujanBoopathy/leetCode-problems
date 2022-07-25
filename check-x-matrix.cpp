44bf2030c6af2ee7a9cc48e1017135db8e88ce94
SujanBoopathy <sujansaradha@gmail.com>
2022-07-25 20:59:57 +0530

    problem-solving

--- check-x-matrix.cpp
+++ check-x-matrix.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     bool checkXMatrix(vector<vector<int>>& grid) {
-        int checker=0;
-        if(grid[0][0]!=0){
-            for(int i=0;i<grid.size();i++){
-                for(int j=0;j<grid.size();j++){
-                    if(i==j && grid[i][j]==checker){
+        
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if((i == j) || (i+j)==(grid.size()-1))
+                {
+                    if(grid[i][j] == 0)
                         return false;
-                    }
-                    if((i+j)==grid.size()-1 && grid[i][j]==checker){
-                        return false;
-                    }
                 }
-            }
-        }
-        else{
-            for(int i=0;i<grid.size();i++){
-                for(int j=0;j<grid.size();j++){
-                    if(i==j && grid[i][j]!=checker){
+                else
+                {
+                    if(grid[i][j] !=0)
                         return false;
-                    }
-                    if((i+j)==grid.size()-1 && grid[i][j]!=checker){
-                        return false;
-                    }
                 }
             }
         }
