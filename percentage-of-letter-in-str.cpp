de704af2c87f7af0cff25aacf7d6942e8cca10c2
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-07-26 19:01:31 +0530

    problem-solving

--- percentage-of-letter-in-str.cpp
+++ percentage-of-letter-in-str.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int percentageLetter(string s, char letter) {
-        
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==letter){
+                count++;
+            }
+        }
+        return floor((count*100)/s.length());
     }
 };
