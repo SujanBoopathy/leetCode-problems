90156d7f6350f4752f740cdb96b34a595ff4fa44
SujanBoopathy <sujansaradha@gmail.com>
2022-07-27 20:23:17 +0530

    problem-solving

--- find-closest-number.cpp
+++ find-closest-number.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int findClosestNumber(vector<int>& nums) {
-        return 0;
+        int res=nums[0],close;
+        if(nums[0]<0){
+            close=nums[0]*-1;
+        }
+        else{
+            close=nums[0];
+        }
+        for(int i=0;i<nums.size();i++){
+            int temp;
+            if(nums[i]<0){
+                temp=nums[i]*-1;
+            }
+            else{
+                temp=nums[i];
+            }
+            if(temp<close)
+                res=nums[i];
+            
+        }
+        return res;
     }
 };
