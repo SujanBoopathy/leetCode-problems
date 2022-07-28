cc7b9cc0ed1f85f3599cc80df0714346294804c9
SujanBoopathy <sujansaradha@gmail.com>
2022-07-28 21:31:05 +0530

    problem-solving

--- min-bit-flips.cpp
+++ min-bit-flips.cpp
@@ -3,6 +3,10 @@ public:
     int minBitFlips(int start, int goal) {
         int n=start^goal;
         int count=0;
+        while (n > 0) {
+            count++;
+            n &= (n - 1);
+        }
         return count;
     }
 };
