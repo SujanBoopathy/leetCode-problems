60d55ba14d56306a28b515657d34f81afa10a6c5
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-08-02 18:59:42 +0530

    Create add-integer.java

---
+++ add-integer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AddInteger{
+   public static void main (String arg[])
+   {
+      Scanner sc = new Scanner (System.in);
+      int num1=sc.nextInt();
+      int num2=sc.nextInt();
+      System.out.println(num1+num2);
+    }
+}
+
+
+
+
+
