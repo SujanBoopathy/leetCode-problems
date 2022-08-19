e861e66d7288ab65ed091eb3b33e7a82c7d153b7
SujanBoopathy <sujansaradha@gmail.com>
2022-08-19 23:12:28 +0530

    problem-solving

--- rearrange-char-to-make-target.java
+++ rearrange-char-to-make-target.java
@@ -2,8 +2,9 @@ class Solution {
     public int rearrangeCharacters(String s, String target) {
         Map<Character,Integer> map1=new HashMap<>();
         Map<Character,Integer> map2=new HashMap<>();
+        int count=0;
         for(int i=0;i<target.length();i++){
-            if(map1.contains(target.charAt(i))){
+            if(map1.containsKey(target.charAt(i))){
                 map1.put(target.charAt(i),map1.get(target.charAt(i))+1);
             }
             else{
@@ -11,13 +12,29 @@ class Solution {
             }
         }
         for(int i=0;i<s.length();i++){
-            if(map2.contains(s.charAt(i))){
-                map1.put(s.charAt(i),map1.get(s.charAt(i))+1);
+            if(map2.containsKey(s.charAt(i))){
+                map2.put(s.charAt(i),map2.get(s.charAt(i))+1);
             }
             else{
                 map2.put(s.charAt(i),1);
             }
         }
-        return s;
+        while(true){      
+            int flag=0;
+            for(Character c: map1.keySet()){
+                if(map2.get(c) >= map1.get(c)){
+                    map2.put(c,map2.get(c)-map1.get(c));
+                }
+                else{
+                    flag=1;
+                }
+            }
+            if(flag==1)
+                break;
+            count++;
+        }
+        
+        return count;
+        
     }
 }
