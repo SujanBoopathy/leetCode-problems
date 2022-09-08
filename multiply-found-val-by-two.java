6a8bc3abfa360286882b6f4fa8a3386d4cbb8249
SujanBoopathy <sujansaradha@gmail.com>
2022-09-08 20:39:47 +0530

    problem-solving

--- multiply-found-val-by-two.java
+++ multiply-found-val-by-two.java
@@ -1,13 +1,13 @@
 class Solution {
     
-    public int findFinalValue(int[] nums, int original) {
+    public int findFinalValue(int[] nums, int search) {
         Set<Integer> set=new HashSet<>();
         for(int i: nums){
             set.add(i);
         }
-        while(set.contains(original)){
-            original*=2;
+        while(set.contains(search)){
+            search*=2;
         }
-        return original;
+        return search;
     }
 }
