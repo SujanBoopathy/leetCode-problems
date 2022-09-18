d70a7c8b1ab87bdea6eb6e68b08bc82cbc6bb969
SujanBoopathy <sujansaradha@gmail.com>
2022-09-18 13:40:59 +0530

    problem-solving

--- min-cost-climbing-stairs.java
+++ min-cost-climbing-stairs.java
@@ -1,51 +1,8 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int minimumCost=0;
-        for(int i=0;i<cost.length;){
-            if(i==0){
-                minimumCost+=cost[i];
-            }
-            if(i==cost.length-1){
-                break;
-            }
-            if(i+2<cost.length){
-                if(cost[i+1]<cost[i+2]){
-                    minimumCost+=cost[i+1];
-                    i++;
-                }
-                else{
-                    minimumCost+=cost[i+2];
-                    i+=2;
-                }
-            }
-            else{
-                break;
-            }
+        for(int i=2;i<cost.length;i++){
+            cost[i]+=Math.min(cost[i-1],cost[i-2]);
         }
-        int minimumCost2=0;
-        for(int i=1;i<cost.length;){
-            if(i==1){
-                minimumCost2+=cost[i];
-            }
-            if(i==cost.length-1){
-                break;
-            }
-            if(i+2<cost.length){
-                if(cost[i+1]<cost[i+2]){
-                    minimumCost2+=cost[i+1];
-                    i++;
-                }
-                else{
-                    minimumCost2+=cost[i+2];
-                    i+=2;
-                }
-            }
-            else{
-                break;
-            }
-        }
-        if(minimumCost2<minimumCost)
-            return minimumCost2;
-        return minimumCost;
+        return Math.min(cost[cost.length-1],cost[cost.length-2]);
     }
 }
