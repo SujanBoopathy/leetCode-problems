ae364140c7ac4fd8299168588c8bc04e639c6efc
SujanBoopathy <sujansaradha@gmail.com>
2022-09-24 20:49:43 +0530

    problem-solving

---
+++ split-str-in-balanced-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int l=0,r=0,count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='R')
+                r++;
+            else
+                l++;
+            if(l==r){
+                count++;
+                l=0;
+                r=0;
+            }
+        }
+        return count;
+    }
+}
