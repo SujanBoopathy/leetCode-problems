536982a9872d9567f4669b43163ab7ccd1eb83ac
SujanBoopathy <sujansaradha@gmail.com>
2022-09-25 19:34:20 +0530

    problem-solving

---
+++ min-operation-to-make-arr-increasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]<nums[i]){
+                continue;
+            }else{
+                count+=(nums[i-1]-nums[i]+1);
+                nums[i]+=nums[i-1]-nums[i]+1;
+            }
+        }
+        return count;
+    }
+}
